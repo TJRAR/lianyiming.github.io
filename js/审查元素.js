46c20cdd2020c958a117c62c34a164b0841a5995
LianYiMing <AGZX_lym@163.com>
2020-09-10 11:01:34 +0800

    1

---
+++ js/审查元素.js
@@ -0,0 +1,13 @@
+
+
+/*
+	 * @name: 审查元素
+	 * @Author: Nekko
+	 * @version: 0.0.1
+	 * @description: 修改网页内容
+	 * @include: *
+	 */
+	$("#test).on('dblclick',function(e){
+	javascript:document.body.contentEditable='true'; document.designMode='on'; void 0};
+	document.getElementsByTagName("html").item(0).appendChild(an);
+		
